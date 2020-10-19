b9dc0cfee21da55f771130127ac7a2019ef78cbf
Marta Obregon <marta.garcia.obregon@.com>
2020-10-19 11:41:59 +0100

    getAge, getHoroscope, edit profile pic and displayed it on dashboard/home

--- models/user.model.js
+++ models/user.model.js
@@ -37,6 +37,9 @@ const userSchema = new Schema(
     },
     phoneNumber: {
       type: Number
+    },
+    image_url:{
+      type: String,
     }
     },
     {
--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,14 +1,68 @@
 const express = require('express');
 const router = express.Router();
 // var bcrypt = require('bcryptjs');
+
 const UserModel = require('../models/User.model')
 
 
 
+function getHoroscope(day, month){
+         
+
+
+  if((month == 1 && day <= 20) || (month == 12 && day >=22)) {
+    return 'Capricorn';
+  } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+    return 'Aquarius';
+  } else if((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+    return 'Pisces';
+  } else if((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+    return 'Aries';
+  } else if((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+    return 'Taurus';
+  } else if((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+    return 'Gemini';
+  } else if((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+    return 'Cancer';
+  } else if((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+    return 'Leo';
+  } else if((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+    return 'Virgo';
+  } else if((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+    return 'Libra';
+  } else if((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+    return 'Scorpio';
+  } else if((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+    return 'Sagittarius';
+  }
+}
+
+function getAge(DOB) {
+  var today = new Date();
+  var birthDate = new Date(DOB);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age = age - 1;
+  }
+  return age;
+}
 router.get('/dashboard/home', (req, res)=>{
-  console.log(req.session)
-  let user = req.session.loggedInUser
-  res.render('dashboard/home.hbs', {user})
+    id = req.session.loggedInUser._id
+    UserModel.findById(id)
+      .then((user)=>{
+        let age = getAge(user.dateOfBirth)
+        var today = new Date(user.dateOfBirth)
+       let day = today.getDay();
+       let month= today.getMonth();
+       let horoscope = getHoroscope(day, month)
+        console.log(age)
+        user.dateofBirth = age
+        user.horoscope = horoscope
+        res.render('dashboard/home.hbs', {user})
+      })
+
+
 })
 
 router.get('/dashboard/edit', (req, res)=>{
@@ -19,33 +73,14 @@ router.get('/dashboard/edit', (req, res)=>{
 
 router.post("/dashboard/edit", (req, res, next) => { 
   const id = { _id: req.session.loggedInUser._id};
-  const { horoscope, occupation, genre, catchPhrase, phoneNumber} = req.body
-
-      function getAge(DOB) {
-        var today = new Date();
-        var birthDate = new Date(DOB);
-        var age = today.getFullYear() - birthDate.getFullYear();
-        var m = today.getMonth() - birthDate.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age = age - 1;
-        }
-        // console.log('today´s date:', today)
-        // console.log('the person age:', age)
-        return age;
-
-      }
-      let dateB = req.session.loggedInUser.dateOfBirth
-
-      let ageVariable = getAge(dateB)
-
+  const { occupation, genre, catchPhrase, phoneNumber, image_url} = req.body
 
   UserModel.findByIdAndUpdate(id, {
-    horoscope,
-    age: ageVariable,
     occupation,
     genre,
     catchPhrase,
     phoneNumber,
+    image_url,
   })
     .then(() => {
       console.log("Data was updated successfully.");
